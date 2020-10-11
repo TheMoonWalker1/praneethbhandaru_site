c64ecb771fc5adb48f0bdd6ed5c38db627f5ce1f
Praneeth Bhandaru <praneethsbhandaru@gmail.com>
2020-10-11 00:43:02 -0400

    Issue: Fixed Link Issue in Director

--- run.sh
+++ run.sh
@@ -2,10 +2,4 @@
 
 source app/bin/activate
 
-pip3 install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('themoonwalker_', 'praneethsbhandaru.com', 'admin')" | python manage.py shell
-
 gunicorn praneethbhandaru.wsgi -b $HOST:$PORT -w 1
