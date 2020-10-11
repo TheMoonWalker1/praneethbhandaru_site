ef446db05e542ef8b3d51dabfbf3ec47d0340038
Praneeth Bhandaru <praneethsbhandaru@gmail.com>
2020-10-11 16:56:12 -0400

    Development: Finished beautification

--- praneethbhandaru/apps/projects/models.py
+++ praneethbhandaru/apps/projects/models.py
@@ -6,6 +6,10 @@ class Project(models.Model):
     date = models.DateField()
     link_to_project = models.CharField(max_length=150, default=None, null=True)
     description = models.TextField()
+    r = models.CharField(max_length=3, default="255")
+    g = models.CharField(max_length=3, default="000")
+    b = models.CharField(max_length=3, default="000")
+    a = models.CharField(max_length=10, default="0.5")
 
     def __str__(self):
         return str(self.title)
--- praneethbhandaru/apps/projects/views.py
+++ praneethbhandaru/apps/projects/views.py
@@ -10,7 +10,15 @@ def projects_view(request):
         
     projects = list(Project.objects.all().order_by('-date'))
     length = len(projects)
-    
-    projects2 = [projects[x:x+3] for x in range(0, length, 3)]
+
+    if request.user_agent.is_mobile:
+        projects2 = [projects[x:x+1] for x in range(0, length, 1)]
+    else:
+        projects2 = [projects[x:x+3] for x in range(0, length, 3)]
+
+    if len(projects2[-1]) < 3:
+        for i in range(len(projects2[-1]), 3):
+            projects2[-1].append(Project.objects.none())
+
     
     return render(request, 'projects.html', {'DOMAIN' : DOMAIN, 'projects' : projects2})
